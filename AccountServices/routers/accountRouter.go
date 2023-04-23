92e5c834cb341a1576fe98b6369778505a4de61d
Miguel-Florian <migflor04@gmail.com>
2023-04-23 18:29:24 +0100

    all routes about account api

---
+++ AccountServices/routers/accountRouter.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"accountservices/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AccountRoute(router *gin.Engine) {
+	//All routes related to book comes here
+
+	api := router.Group("/api/accountservices")
+	{
+		api.POST("/createaccount", controllers.CreateAccount())
+		api.GET("/accounts", controllers.GetAccounts())
+		api.GET("/account/:accountnumber", controllers.GetAccount())
+		api.GET("/account/:accountnumber/solde", controllers.GetSoldeAccount())
+		api.DELETE("/account/:accountnumber", controllers.DeleteAccount())
+	}
+
+}
