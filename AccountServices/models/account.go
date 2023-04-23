baa5b71eabfe8660f2b3f156e86dc7f47fd63e71
Miguel Florian Noumowe <71710417+Miguel-Florian@users.noreply.github.com>
2023-04-23 18:21:46 +0100

    account data model

---
+++ AccountServices/models/account.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Account struct {
+	ID             primitive.ObjectID `json:"id,onitempty" bson:"_id,onitempty"`
+	Number_account string             `json:"account_number,onitempty" bson:"account_number,onitempty,unique"`
+	Type_account   string             `json:"account_type,onitempty" bson:"account_type,onitempty"`
+	Amount         int64              `json:"amount,onitempty" bson:"amount,onitempty"`
+	DateCreation   time.Time          `json:"dateCreation,onitempty" bson:"dateCreation,onitempty"`
+	DateUpdate     time.Time          `json:"dateUpdate,onitempty" bson:"dateUpdate,onitempty"`
+	FirstName      string             `json:"firstname,onitempty" bson:"firstname,onitempty"`
+	LastName       string             `json:"lastname,onitempty,unique" bson:"lastname,onitempty"`
+	Email          string             `json:"email,onitempty" bson:"email,onitempty,unique"`
+	Adresse        string             `json:"adresse,onitempty" bson:"adresse,onitempty,unique"`
+	Phone          string             `json:"phone,omnitempty" bson:"phone,onitempty,unique"`
+	Password       string             `json:"password" bson:"password,onitempty"`
+}
