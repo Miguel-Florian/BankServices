2f0e0fc15ecbff7910e93ce34588b72718d5d714
Miguel-Florian <migflor04@gmail.com>
2023-04-24 18:35:24 +0100

    main file

---
+++ TransactionServices/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"transactionservices/config"
+	"transactionservices/router"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Starting Transaction server ...")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
+
+	r := gin.Default()
+
+	r.RedirectTrailingSlash = true
+	r.RedirectFixedPath = true
+
+	//run database server
+	config.ConnectDB()
+
+	router.TransactionStorageRoute(r)
+
+	r.SetTrustedProxies(nil)
+	r.Run(PORT)
+}
