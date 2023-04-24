292dee6e0d0bd2ec5a3bb1bc3e1a5e9c4eda5974
Miguel-Florian <migflor04@gmail.com>
2023-04-24 14:59:18 +0100

    transaction model

---
+++ TransactionServices/model/deposit.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Transaction struct {
+	ID             primitive.ObjectID `json:"id,onitempty" bson:"_id,onitempty"`
+	Number_account string             `json:"account_number,onitempty" bson:"account_number,onitempty,unique"`
+	Amount         int64              `json:"amountDeposit,onitempty" bson:"amountDeposit,onitempty"`
+	Date           time.Time          `json:"date,onitempty" bson:"date,onitempty"`
+}
